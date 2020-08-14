5eba014a09151a8639a4ca2f3b6f44193de4a946
Sam Huang <63796156+k612created@users.noreply.github.com>
2020-08-14 22:03:24 +0800

    Update zoknapsack.h

--- includes/zoknapsack.h
+++ includes/zoknapsack.h
@@ -2,8 +2,8 @@
 #define _ZERO_ONE_KNAPSACK_SOLVING_H
 
 #define InitZeroOneSolve(type1) \
-	typedef type1 (*Function_Loading)(type1, type1); \
-	type1 ZeroOneSolve_##type1(type1 _N, type1 _M, type1* _Result, type1* _Value, type1* _Weight, Function_Loading func) { \
+	typedef type1 (*Function_Loading##type1)(type1, type1); \
+	type1 ZeroOneSolve_##type1(type1 _N, type1 _M, type1* _Result, type1* _Value, type1* _Weight, Function_Loading##type1 func) { \
 		_Result[0] = 0; \
 		for (type1 i = 0; i < _N; i++) { \
 			for (type1 j = _N; j >= _Value[i]; j--) { \
